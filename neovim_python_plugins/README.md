79a2c774f2d6cc11669f5697d3f65a60007d65b3
schef <zvanstefan@gmail.com>
2021-02-09 14:39:26 +0100

    added python plugion

---
+++ neovim_python_plugins/README.md
@@ -0,0 +1,5 @@
+The inspiration was taken from http://blog.tpleyer.de/posts/2019-02-20-Python-Plugin-for-Neovim.html
+
+pip install pynvim --user
+mkdir -p $HOME/.config/nvim/rplugin/python
+cp formatjson.py $HOME/.config/nvim/rplugin/python/
---
+++ neovim_python_plugins/formatjson.py
@@ -0,0 +1,32 @@
+import re
+import pynvim
+
+@pynvim.plugin
+class Translator(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.command('JsonFold', range=True, nargs='*', sync=True)
+    def command_handler(self, args, range):
+        bracketCount = 0
+        oneLine = ""
+        shouldDecrese = False
+        lines = []
+        self.nvim.feedkeys("gg=G")
+        for el, line in enumerate(self.nvim.current.buffer):
+            for ec, char in enumerate(line):
+                if (char == "{"):
+                    bracketCount += 1
+                elif (char == "}"):
+                    bracketCount -= 1
+            if (bracketCount == 4 and "{" in line):
+                    oneLine += line
+            elif (bracketCount == 3 and "}" in line):
+                    oneLine += line.lstrip()
+                    lines.append(oneLine)
+                    oneLine = ""
+            elif (bracketCount >= 4):
+                    oneLine += line.lstrip()
+            else:
+                lines.append(line)
+        self.nvim.current.buffer[:] = lines
